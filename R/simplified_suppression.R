4ee85c1042dfb898a0ced0f317f9952afe7966e4
thomas.hepworth@justice.gov.uk <thomas.hepworth@justice.gov.uk>
2022-01-17 12:59:55 +0000

    fix simplified_supp function

--- R/simplified_suppression.R
+++ R/simplified_suppression.R
@@ -42,6 +42,7 @@
 #' @include priority_suppression.R
 #' @include pseudo_suppression.R
 #' @include total_suppressors.R
+#' @include apply_single_group_suppression.R
 #' 
 #' 
 #' @importFrom magrittr "%>%"
@@ -81,6 +82,7 @@ simple_suppression <- function(
 ) {
   
   # set defaults for various removed features
+  subset_df_along_row_and_col_arguments = TRUE 
   columns_to_pseudo_suppress = NULL
   rows_to_pseudo_suppress = NULL
   pseudo_suppression_threshold = NULL
@@ -88,113 +90,31 @@ simple_suppression <- function(
   priority_row_suppression = NULL
   ordered_priority_suppression = FALSE
   suppression_pre_applied = FALSE
-  suppression_total_suppression = FALSE
-  subset_df_along_row_and_col_arguments = TRUE 
+  total_suppression = FALSE
   
-  # if no cols are entered for suppression, select all numeric cols
-  if(is.null(cols_to_suppress)) {
-    cols_to_suppress <- df %>% 
-      dplyr::select_if(is.numeric) %>% 
-      colnames()
-  }
-  # if no rows are entered for suppression, select all available rows
-  if(is.null(row_nos_to_suppress)) {
-    row_nos_to_suppress <- 1:nrow(df)
-  }
-  
-  # find the row index to use for our priority row suppression (if applicable)
-  if(is.null(priority_row_suppression)) {
-    row_index_to_use <- NULL
-  } else {
-    row_index_to_use <- priority_row_suppression-(min(row_nos_to_suppress)-1)
-  }
-  
-  # if no pseudo supp value is entered, set this to our supp threshold
-  if(is.null(pseudo_suppression_threshold)) pseudo_suppression_threshold <- suppression_thres
-  
-  # split out our original, unsuppressed df to use later in this process
-  unsuppressed_df <- df
-  
-  # initially, work out whether we're pseudo suppressing 0
-  if(is.null(pseudo_suppress_0)) {
-    pseudo_suppress_0 <- suppress_0
-  }
-  
-  # initially, subset df if required (so we only suppress across selected row/col combos)
-  df_to_suppress <- df
-  
-  if(subset_df_along_row_and_col_arguments) {
-    df_to_suppress <- df_to_suppress[,cols_to_suppress] # subset cols (keep only numeric cols...)
-    if(!is.null(row_nos_to_suppress)) { # subset rows only if we're working with rows
-      df_to_suppress <- df_to_suppress[row_nos_to_suppress,]
-    }
-  }
-  
-  # ensure we don't run into any errors...
-  df_to_suppress <- df_to_suppress %>% 
-    dplyr::mutate_if(is.numeric, as.double) %>% 
-    dplyr::as_tibble()
-  # stop the code from running if we run into mismatches between data types
-  if(!all(sapply(df_to_suppress, typeof) == "double")) {
-    stop("Not all of our subset df's columns are doubles. Please review the data being input before continuing.")
-  }
-  
-  # Apply specific suppression to required locations, based on how whether we have "row", "col" or both
-  if("col" %in% where_to_suppress) { # if we have column suppression that needs applying, always apply primary
-    if(!suppression_pre_applied) {
-      #apply primary suppression and then use these results to apply secondary suppression
-      df_to_suppress <- apply_primary_suppression_cols(df_to_suppress, 
-                                                       cols_to_suppress,
-                                                       suppression_thres,
-                                                       suppress_0)
-    }
-  }
-  
-  if(
-    all("col" == where_to_suppress) & # if we're including secondary suppression, and this is only on columns, apply regular column secondary supp
-    inc_secondary_suppression & 
-    is.null(priority_row_suppression)
-  ) {
-    df_to_suppress <- df_to_suppress %>% 
-      dplyr::mutate_at(cols_to_suppress, 
-                       ~apply_secondary_suppression_cols(.x,
-                                                         secondary_suppress_0))
-  } else if(
-    all("row" == where_to_suppress) # if we're only including row suppression, apply primary and secondary suppression according to arguments
-  ){
-    df_to_suppress <- apply_suppression_rows(df_to_suppress, 
-                                             cols_to_suppress,
-                                             row_nos_to_suppress,
-                                             suppression_thres,
-                                             suppress_0,
-                                             inc_secondary_suppression,
-                                             inc_primary_suppression = !suppression_pre_applied,  # invert input
-                                             secondary_suppress_0)
-  } else if(
-    all(c("col", "row") %in% where_to_suppress & inc_secondary_suppression) # otherwise, if we're suppressing both rows and columns, we need to apply some fancier suppression (function family cross-referenced suppression)
-    # this looks to check where previous suppression has occurred and attempts to put any required secondary suppression in corresponding gaps, so we don't oversuppress
-  ){
-    # primary suppression applied in an earlier stage, so just fill in any gaps created
-    df_to_suppress <- apply_cross_referenced_suppression(df_to_suppress,
-                                                         columns_to_suppress = cols_to_suppress,
-                                                         priority_row_suppression = row_index_to_use,
-                                                         ordered_priority_suppression = ordered_priority_suppression,
-                                                         secondary_suppress_0 = secondary_suppress_0)
-  }
-  
-  # unlist if required
-  if(is_list(df_to_suppress)) df_to_suppress <- df_to_suppress[[1]]
-  
-  # bind our subset values back onto the original (if required)
-  if(subset_df_along_row_and_col_arguments) {
-    # subset our df accordingly
-    df[row_nos_to_suppress,cols_to_suppress] <- df_to_suppress
-  }
-  
-  #set value to output where suppression has occurred (default is to leave it as is)
-  df <- df %>% 
-    dplyr::mutate_at(cols_to_suppress,
-              ~ifelse(. == 9999999, suppression_output_value, .))
+  # use our single group suppression and fill in some
+  # arguments automatically
+  df <- suppress_single_group(
+    df,
+    where_to_suppress = where_to_suppress,
+    cols_to_suppress = NULL,
+    row_nos_to_suppress = NULL,
+    suppression_thres,
+    suppress_0 = TRUE,
+    inc_secondary_suppression = TRUE,
+    suppression_output_value = 9999999,
+    subset_df_along_row_and_col_arguments = subset_df_along_row_and_col_arguments,
+    columns_to_pseudo_suppress = columns_to_pseudo_suppress,
+    rows_to_pseudo_suppress = rows_to_pseudo_suppress,
+    pseudo_suppression_threshold = pseudo_suppression_threshold,
+    pseudo_suppress_0 = pseudo_suppress_0,
+    priority_row_suppression = priority_row_suppression,
+    ordered_priority_suppression = ordered_priority_suppression,
+    suppression_pre_applied = suppression_pre_applied,
+    total_suppression = total_suppression,
+    indirect_suppression = FALSE,
+    secondary_suppress_0 = TRUE
+  )
   
   return(df)
 }
